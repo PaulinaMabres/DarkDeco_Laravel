1b98e3970d1b82e214946c31bbb5cd5f2751c795
mlucasflores <ifloreslucas@gmail.com>
2019-07-01 22:33:16 -0300

    Agrego solo ruta home y los css/imagenes/favicon
    No funciona un script que después lo veo yo (Lucas)
    Agregué solo una ruta; falta agregar el 'php artisan make:auth'

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,83 @@
+<footer>
+    <section id="footer">
+
+      <?php // IDEA: Se comenta barra de footer ?>
+       <div class="container-fluid siempreAbajo">
+
+      <!--  <div class="row text-center text-xs-center text-sm-left text-md-left">
+
+          <div class="col-xs-12 col-sm-4 col-md-3 offset-md-2"><h5>Navegación</h5>
+            <ul class="list-unstyled links ">
+              <li><a href="home.php"><i class="fa fa-angle-double-right"></i>Home</a></li>
+              <li><a href="register.php"><i class="fa fa-angle-double-right"></i>Register</a></li>
+              <li><a href="faq.php"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+              <li><a href="login.php"><i class="fa fa-angle-double-right"></i>Log In</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Extra link</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-12 col-sm-4 col-md-3"> <h5>Mi cuenta</h5>
+            <ul class="list-unstyled links">
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Mis pedidos</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Mis datos personales</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Mis favoritos</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Mi historial</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-12 col-sm-4 col-md-3"> <h5> Ayuda </h5>
+            <ul class="list-unstyled links">
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Comprar</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Centro de seguridad</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Centro de atencion al cliente</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Resolucion de problemas</a></li>
+              <li><a href="#"><i class="fa fa-angle-double-right"></i>Extra link</a></li>
+            </ul>
+          </div>
+        </hr>
+      </div> -->
+
+      <!-- Redes Sociales -->
+      <div class="row">
+  				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+            <div class="col-md-6 col-sm-6">
+      					<ul class="list-unstyled list-inline social text-center text-white">
+                  <h4>Seguinos en nuestras redes y contactanos!</h4>
+      						<li class="list-inline-item"><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f fa-2x"></i> </a></li>
+      						<li class="list-inline-item"><a href="https://twitter.com" target="_blank"><i class="fab fa-twitter fa-2x"></i> </a></li>
+      						<li class="list-inline-item"><a href="https://instagram.com" target="_blank"><i class="fab fa-instagram fa-2x"></i> </a></li>
+      						<li class="list-inline-item"><a href="https://googleplus.com" target="_blank"><i class="fab fa-google-plus fa-2x"></i>  </a></li>
+      						<li class="list-inline-item"><a href="mailto:ejemplo@email.com"><i class="fa fa-envelope fa-2x"></i>  </a></li>
+  					    </ul>
+            </div>
+            <div class="col-md-6 col-sm-6 text-white">
+              <ul class="list-unstyled list-inline social text-center text-white">
+              <h4>Sumate a nuestro Newsletter!</h4>
+                <div class="cajaEmail d-flex justify-content-center">
+                  <i class="fas fa-paper-plane"></i>
+                  <input type="email" name="" value="" placeholder="ingresa tu e-mail" class="cajaTexto">
+                  <button type="button" name="button" class="text-dark boton">No funciona</button>
+                </div>
+              </ul>
+            </div>
+  				</div>
+  				</hr>
+  		</div>
+    <!-- Extra Footer -->
+      <div class="row extra-footer">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+          <p class="h6">
+            <span> &bull; &copy; Derechos Reservados Black Décor &nbsp; &bull; Los precios están en pesos argentinos </span> &nbsp;
+            <a href="#"> &bull; Terminos y condiciones </a> &nbsp;
+            <a href="#"> &bull; Privacidad</a>
+
+          </p>
+          <p class="letraChica">
+              Hecho con mucho <i class="fas fa-heart"></i> & <i class="fas fa-coffee"></i> por Paulina, Lucas y Agostina.
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,97 @@
+<section id="header" class="header">
+
+  <nav class="navbar navbar-expand-sm  navbar-dark bg-dark container-fluid color fixed-top anchura ml-0 ">
+
+    <a class="navbar-brand mt-15 " href="home.php">
+      <img src="images/logoNuevoRecortado.png" title="DarkDeco" class="d-inline-block align-top logo" alt="logoDarkD">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="navbar-nav mr-auto ml-auto text-center">
+        <a class="nav-item nav-link active h4 ml-3" href="home.php">Home</a>
+
+        <!-- IDEA: Categorias
+         <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle h4 ml-3 p-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Categorías
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#"><center>Sillas</center></a>
+            <a class="dropdown-item" href="#"><center>Mesas</center></a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#"> <center>Promociones</center></a>
+          </div>
+        </li> -->
+
+        <a class="nav-item nav-link h4 ml-3" href="faq.php">FAQ</a>
+        <!-- IDEA: LINK extra
+        <a class="nav-item nav-link h4 ml-3" href="#"> Extra </a> -->
+      </div>
+
+      <!-- IDEA: LOGIN collapse -->
+
+      <div class="login-collapse navbar-nav d-flex flex-row justify-content-around">
+            <?php
+            // IDEA: Todo esto hay que descomentarlo despues IDEA
+
+            // if (session_status() == PHP_SESSION_NONE) {
+            //     session_start();
+            // }
+            // if( isset($_SESSION['logueado']) &&  $_SESSION['logueado']
+            //     && isset($_COOKIE['UsuarioLogueado']) && $_COOKIE['UsuarioLogueado'] == true )
+            // {
+            //   // IDEA: SI el usuario está logueado:
+            //   echo '
+            //
+            //
+            //   <li class="nav-item dropdown btn-group dropleft">
+            //     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            //       <i class="fas fa-user-circle fa-2x"></i>
+            //     </a>
+            //     <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
+            //       <h6>&nbsp Bienvenido, <strong>';
+            //
+            //         echo $_SESSION['nombreGuardado'];
+            //
+            //       echo'</strong> </h6>
+            //       <a class="dropdown-item" href="logout.php"><button class="btn btn-outline-danger" type="submit">Log out</button></a>
+            //       <div class="dropdown-divider"></div>
+            //       <a class="dropdown-item" href="edit-usuario.php">Modificar perfil</a>
+            //     </div>
+            //   </li>
+            //
+            //   </div>';
+            // }
+            // else
+            // {
+            //   // IDEA: SI no lo está:
+            //   echo '
+            //   <a class="btn btn-outline-warning mr-3 h4" href="register.php">Registrate</a>
+            //   <a class= "btn text-white mr-3 h4 " href="login.php">Iniciar sesión</a>
+            //   ';
+            // }
+            // ?>
+        </div>
+
+
+
+      <!-- IDEA: CARRITO; se comenta porque no funciona -->
+      <!-- <a class=" nav-link text-secondary d-flex justify-content-center mb-2"href="#"><i class="fas fa-shopping-cart fa-2x"></i>
+      </a> -->
+
+      <?php // IDEA: Se comenta porque no funciona [Boton de busqueda ] ?>
+      <!-- <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Búsqueda" aria-label="Search">
+        <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Buscar</button>
+      </form> -->
+
+
+    </div>
+
+  </nav>
+</section>
